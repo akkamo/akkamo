046295bf6470c081950590786660da4b3be5afdc
Vaclav Svejcar <vaclav.svejcar@finfactory.com>
2016-05-11 13:18:18 +0200

    New Typesafe Config generic helper methods

--- makka/src/main/scala/com/github/jurajburian/makka/package.scala
+++ makka/src/main/scala/com/github/jurajburian/makka/package.scala
@@ -9,45 +9,85 @@ import scala.util.Try
 	* @author jubu
 	*/
 package object config {
+
 	import scala.collection.JavaConversions._
 
-	def blockAsMap(key:String)(implicit cfg:Config):Option[Map[String, Config]] = Try {
+	def blockAsMap(key: String)(implicit cfg: Config): Option[Map[String, Config]] = Try {
 
 		val keys = cfg.getObject(key).keySet()
-		keys.map{p=>(p, cfg.getConfig(s"$key.$p"))}.toMap.map(p=>(p._1, p._2.resolve()))
+		keys.map { p => (p, cfg.getConfig(s"$key.$p")) }.toMap.map(p => (p._1, p._2.resolve()))
 	}.toOption
 
-	def getStringList(path:String)(implicit cfg:Config):Option[List[String]] =  {
-		if(cfg.hasPath(path)) {
+	def getStringList(path: String)(implicit cfg: Config): Option[List[String]] = {
+		if (cfg.hasPath(path)) {
 			Some(cfg.getStringList(path).toList)
 		} else {
 			None
 		}
 	}
 
-	def getInt(path:String)(implicit cfg:Config):Option[Int] =  {
-		if(cfg.hasPath(path)) {
+	def getInt(path: String)(implicit cfg: Config): Option[Int] = {
+		if (cfg.hasPath(path)) {
 			Some(cfg.getInt(path))
 		} else {
 			None
 		}
 	}
 
-	def getString(path:String)(implicit cfg:Config):Option[String] =  {
-		if(cfg.hasPath(path)) {
+	def getString(path: String)(implicit cfg: Config): Option[String] = {
+		if (cfg.hasPath(path)) {
 			Some(cfg.getString(path))
 		} else {
 			None
 		}
 	}
 
-	def getBoolean(path:String)(implicit cfg:Config):Option[Boolean] =  {
-		if(cfg.hasPath(path)) {
+	def getBoolean(path: String)(implicit cfg: Config): Option[Boolean] = {
+		if (cfg.hasPath(path)) {
 			Some(cfg.getBoolean(path))
 		} else {
 			None
 		}
 	}
 
+	private def getInternal[T](path: String, cfg: Config, t: Transformer[T]): Option[T] = {
+		if (cfg.hasPath(path)) {
+			Some(t.transform(cfg, path))
+		} else {
+			None
+		}
+	}
 
+	def get[T](key: String, cfg: Config)(implicit t: Transformer[T]): Option[T] = {
+		getInternal[T](key, cfg, t)
+	}
+
+
+	def get[T](key: String)(implicit t: Transformer[T], cfg: Config): Option[T] = {
+		getInternal[T](key, cfg, t)
+	}
+
+	implicit val cfg2Int = new Transformer[Int] {
+		override def transform(cfg: Config, key: String): Int = cfg.getInt(key)
+	}
+
+	implicit val cfg2String = new Transformer[String] {
+		override def transform(cfg: Config, key: String): String = cfg.getString(key)
+	}
+
+	implicit val cfg2StringList = new Transformer[List[String]] {
+		override def transform(cfg: Config, key: String): List[String] = cfg.getStringList(key).toList
+	}
+
+	implicit val cfg2Config = new Transformer[Config] {
+		override def transform(cfg: Config, key: String): Config = cfg.getConfig(key)
+	}
+
+	implicit val cfg2Boolean = new Transformer[Boolean] {
+		override def transform(cfg: Config, key: String): Boolean = cfg.getBoolean(key)
+	}
+}
+
+trait Transformer[T] {
+	def transform(cfg: Config, key: String): T
 }
