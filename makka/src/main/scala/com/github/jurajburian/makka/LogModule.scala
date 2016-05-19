c19cc0159b31fabeb3b9cc45a8ee8ffe546a6556
jubu <jurajburian@gmail.com>
2016-05-19 12:18:40 +0200

    LogModule simplified implementation

--- makka/src/main/scala/com/github/jurajburian/makka/LogModule.scala
+++ makka/src/main/scala/com/github/jurajburian/makka/LogModule.scala
@@ -2,9 +2,6 @@ package com.github.jurajburian.makka
 
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
-import com.typesafe.config.Config
-
-import scala.util.Try
 
 /**
 	* factory provided by `LogModule`
@@ -37,22 +34,16 @@ trait LoggingAdapterFactory {
 	*/
 class LogModule extends Module with Initializable {
 
-	val LoggingActorSystem = "akka.logging.actorSystem"
+	val LoggingActorSystem = "logging"
 
-	override def initialize(ctx: Context): Boolean = ctx.inject[Config] match {
-		case Some(cfg) => {
-			val actorSystem = Try(cfg.getString(LoggingActorSystem)).toOption.map(ctx.inject[ActorSystem](_)).getOrElse(ctx.inject[ActorSystem])
-			actorSystem match {
-				case Some(as)=> {
-					ctx.register[LoggingAdapterFactory](new LoggingAdapterFactory {
-						override def apply[T](category: Class[T]): LoggingAdapter = Logging(as, category)
-						override def apply(category: AnyRef): LoggingAdapter = Logging(as, category.getClass)
-					})
-					true
-				}
-				case _ => false // actor system is not configured, postpone initialization
-			}
-		}
-		case _ => false // no config postpone initialization
+	override def initialize(ctx: Context): Boolean = {
+		if(ctx.initialized[AkkaModule]) {
+			val actorSystem = ctx.inject[ActorSystem](LoggingActorSystem).getOrElse(throw InitializationError("Can't find any Actor Suystem for logger"))
+			ctx.register[LoggingAdapterFactory](new LoggingAdapterFactory {
+				override def apply[T](category: Class[T]): LoggingAdapter = Logging(actorSystem, category)
+				override def apply(category: AnyRef): LoggingAdapter = apply(category.getClass)
+			})
+			true
+		} else {false}
 	}
 }
