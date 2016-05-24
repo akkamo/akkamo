9eed3f445b473c183255b7102b5c35d2cf55ed33
Vaclav Svejcar <vaclav.svejcar@finfactory.com>
2016-05-24 12:54:15 +0200

    ST-405: better Scaladoc for Disposable trait

--- makka/src/main/scala/com/github/jurajburian/makka/Disposable.scala
+++ makka/src/main/scala/com/github/jurajburian/makka/Disposable.scala
@@ -1,20 +1,32 @@
 package com.github.jurajburian.makka
 
-/** The last module lifecycle signature
- *
- * @author jubu
- */
+/**
+	* Trait indicating that the module extending this requires to perform additional actions before
+	* the ''Makka'' shutdown. This is the very last stage of the ''Makka'' module lifecycle and in
+	* this stage, module should close all opened connection, files and get prepared for shutdown.
+	*
+	* @author jubu
+	*/
 trait Disposable {
 
-  //TODO better message
-	@throws[DisposableError]("If dispose execution fails")
+	/**
+		* Method the module extending this trait must implement, all logic performed by the module
+		* before ''Makka'' system shutdown should be here. As an input parameter, ''Makka'' context is
+		* provided.
+		*
+		* @param ctx ''Makka'' context
+		* @throws DisposableError thrown in case of serious unrecoverable error, occured during the
+		*                         dispose stage
+		*/
+	@throws[DisposableError]("If serious unrecoverable problem during dispose stage occurs")
 	def dispose(ctx:Context):Unit
 
 }
 
 /**
- *
- * @param message
- * @param cause
- */
+	* Error to be thrown in case of serious unrecoverable error, occured during the dispose stage.
+	*
+	* @param message error message
+	* @param cause   optional value of cause
+	*/
 case class DisposableError(message:String, cause:Throwable = null) extends Error(message, cause)
