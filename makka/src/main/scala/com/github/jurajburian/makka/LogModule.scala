3c9ac45312d01ec425e930336f26baa586b764bb
Vaclav Svejcar <vaclav.svejcar@finfactory.com>
2016-05-25 15:08:19 +0200

    Fixed broken Scaladoc

--- makka/src/main/scala/com/github/jurajburian/makka/LogModule.scala
+++ makka/src/main/scala/com/github/jurajburian/makka/LogModule.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 
 /**
-	* Simple factory, providing [[LoggingAdapter]] instance for specified category (e.g. module name,
+	* Simple factory, providing `LoggingAdapter` instance for specified category (e.g. module name,
 	* selected class). This factory is registered by the [[LogModule]] into the ''Makka'' context.
 	*
 	* @author jubu
@@ -12,21 +12,21 @@ import akka.event.{Logging, LoggingAdapter}
 trait LoggingAdapterFactory {
 
 	/**
-		* Returns instance of [[LoggingAdapter]] for specified category (e.g. module name,
+		* Returns instance of `LoggingAdapter` for specified category (e.g. module name,
 		* selected class).
 		*
-		* @param category category for which the [[LoggingAdapter]] will be returned
+		* @param category category for which the `LoggingAdapter` will be returned
 		* @tparam T type of the category class
-		* @return instance of [[LoggingAdapter]]
+		* @return instance of `LoggingAdapter`
 		*/
 	def apply[T](category: Class[T]): LoggingAdapter
 
 	/**
-		* Returns instance of [[LoggingAdapter]] for specified category (e.g. module name,
+		* Returns instance of `LoggingAdapter` for specified category (e.g. module name,
 		* selected class).
 		*
-		* @param category category for which the [[LoggingAdapter]] will be returned
-		* @return instance of [[LoggingAdapter]]
+		* @param category category for which the `LoggingAdapter` will be returned
+		* @return instance of `LoggingAdapter`
 		*/
 	def apply(category: AnyRef): LoggingAdapter
 }
