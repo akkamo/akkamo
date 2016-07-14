1245f347941f5b6feed70abbb6a6203d34f22fc9
jubu <jurajburian@gmail.com>
2016-07-14 09:04:03 +0200

    More friendly API

--- akkamo/src/main/scala/eu/akkamo/Context.scala
+++ akkamo/src/main/scala/eu/akkamo/Context.scala
@@ -50,7 +50,7 @@ trait Context {
     *
     * @param ct ''Akkamo'' context
     * @tparam T type of the service
-    * @return implementation of interface `T` if initialized
+    * @return implementation of interface `T` encapsulated in [[scala.Some]] if exists else [[scala.None]]
     */
   def inject[T](implicit ct: ClassTag[T]): Option[T]
 
@@ -65,9 +65,41 @@ trait Context {
     *               under key then default instance is returned if it exists and is initialized
     * @param ct     class tag evidence
     * @tparam T type of the service
-    * @return implementation of interface `T` if initialized
+    * @return implementation of interface `T` encapsulated in [[scala.Some]] if exists else [[scala.None]]
     */
   def inject[T](key: String, strict: Boolean = false)(implicit ct: ClassTag[T]): Option[T]
+
+
+  /**
+    * Get service already registered as an ''default'' service in ''Akkamo'' context.
+    *
+    * @param ct ''Akkamo'' context
+    * @tparam T type of the service
+    * @throws ContextError if bean not exists
+    * @return implementation of interface `T`
+    */
+  @throws[ContextError]
+  def get[T](implicit ct: ClassTag[T]): T = inject[T]
+    .getOrElse(throw new ContextError(s"Can't find registered bean of type: ${ct.runtimeClass.getName}"))
+
+  /**
+    * Get service already registered in ''Akkamo'' context, using the ''key'' using which the
+    * service has been registered. By default, if no service is found with this key, ''default''
+    * service is returned (if available). To disable this behavior, set the `strict` parameter to
+    * `false`.
+    *
+    * @param key    additional mapping identifier
+    * @param strict if `true`, only named instance is returned if false then when nothing found
+    *               under key then default instance is returned if it exists and is initialized
+    * @param ct     class tag evidence
+    * @tparam T type of the service
+    * @throws ContextError if bean not exists
+    * @return implementation of interface `T`
+    */
+  @throws[ContextError]
+  def get[T](key: String, strict: Boolean = false)(implicit ct: ClassTag[T]): T = inject[T](key, strict)
+    .getOrElse(throw new ContextError(s"Can't find registered bean under key: $key of type: ${ct.runtimeClass.getName}"))
+
 
 
   /**
@@ -85,14 +117,17 @@ trait Context {
 
 
   /**
-    * get mapping from services to set of aliases
+    * register bean `x` to previously `registered` bean of type `T`
     *
-    * @param ct
-    * @tparam T
-    * @return map of all services of type `T` to set of aliases
+    * @param x   registered instance
+    * @param key mapping identifier of previously registered instance of type `T`
+    * @param ct  class tag evidence
+    * @tparam T type of `registered` registered object
+    * @tparam X type of instance going to be registered
+    * @return updated instance of `Context`
     */
-  def registered[T <: AnyRef](implicit ct: ClassTag[T]): Map[T, Set[String]]
-
+  @throws[ContextError]
+  def registerIn[T <: Registry[X], X](x: X, key: String)(implicit ct: ClassTag[T]): Context = registerIn[T, X](x, Some(key))
 
   /**
     * register bean
@@ -105,6 +140,27 @@ trait Context {
     */
   def register[T <: AnyRef](value: T, key: Option[String] = None)(implicit ct: ClassTag[T]): Context
 
+
+  /**
+    * register bean
+    *
+    * @param value
+    * @param key
+    * @param ct class tag evidence
+    * @tparam T
+    * @return updated instance of `Context`
+    */
+  def register[T <: AnyRef](value: T, key: String)(implicit ct: ClassTag[T]): Context = register[T](value, Some(key))
+
+
+  /**
+    * get mapping from services to set of aliases
+    *
+    * @param ct
+    * @tparam T
+    * @return map of all services of type `T` to set of aliases
+    */
+  def registered[T <: AnyRef](implicit ct: ClassTag[T]): Map[T, Set[String]]
 }
 
 
