c380103aed6d3bbb7595b35ea97233a2d3aa2bcb
Vaclav Svejcar <vaclav.svejcar@finfactory.com>
2016-08-29 12:16:47 +0200

    Completely removing custom logging mechanism from AkkaHttp module

--- akkamoAkkaHttp/src/main/scala/eu/akkamo/AkkaHttpModule.scala
+++ akkamoAkkaHttp/src/main/scala/eu/akkamo/AkkaHttpModule.scala
@@ -4,18 +4,15 @@ import java.security.{KeyStore, SecureRandom}
 import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 
 import akka.actor.ActorSystem
-import akka.event.{Logging, LoggingAdapter}
+import akka.event.LoggingAdapter
 import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.RouteResult.{Complete, Rejected}
-import akka.http.scaladsl.server.directives.{DebuggingDirectives, LogEntry, LoggingMagnet}
 import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
 import akka.stream.ActorMaterializer
 import com.typesafe.config.Config
 import eu.akkamo.RouteRegistry.{HTTP, HTTPS, Protocol}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 import scala.util.Try
 
 /**
@@ -105,8 +102,6 @@ object RouteRegistry {
   *       protocol = "http" // http, https, ...
   *       host = "localhost" // host, default localhost
   *       akkaAlias = "alias" // not required, default is used if exists
-  *       requestLogLevel = "info"  // defines level for request level logging. Default "off" means no logging
-  *       requestLogFormat = "%{HTTP_METHOD} %{REQ_URI}: HTTP/%{RESP_STATUS}" // defines log format, defaults to this if not specified
   *     },
   *     // configuration registered as default (only one instance is allowed)
   *     name2 = {
@@ -136,10 +131,6 @@ class AkkaHttpModule extends Module with Initializable with Runnable with Dispos
   private val Port = "port"
 
   private val Interface = "interface"
-
-  private val RequestLogLevel = "requestLogLevel"
-
-  private val RequestLogFormat = "requestLogFormat"
 
   private val AkkaAlias = "akkaAlias"
 
@@ -187,18 +178,13 @@ class AkkaHttpModule extends Module with Initializable with Runnable with Dispos
 
   private[AkkaHttpModule] case class
   HttpRouteRegistry(aliases: List[String], port: Int, interface: String, default: Boolean,
-                    requestLogLevel: String, requestLogFormat: String, routes: Set[Route] = Set.empty)
+                    routes: Set[Route] = Set.empty)
                    (implicit as: ActorSystem) extends BaseRouteRegistry {
 
     def bind(route: Route): Future[ServerBinding] = {
       implicit val am = ActorMaterializer()
       import scala.concurrent.ExecutionContext.Implicits.global
-      if (requestLogLevel == "off") {
-        Http().bindAndHandle(route, interface, port)
-      } else {
-        val myLoggedRoute = logRequestResult(requestLogLevel, requestLogFormat, route)
-        Http().bindAndHandle(myLoggedRoute, interface, port)
-      }
+      Http().bindAndHandle(route, interface, port)
     }
 
     def copyWith(p: Route) = {
@@ -210,19 +196,13 @@ class AkkaHttpModule extends Module with Initializable with Runnable with Dispos
 
   private[AkkaHttpModule] case class
   HttpsRouteRegistry(aliases: List[String], port: Int, interface: String, default: Boolean,
-                     ctx: HttpsConnectionContext, requestLogLevel: String, requestLogFormat: String,
-                     routes: Set[Route] = Set.empty)
+                     ctx: HttpsConnectionContext, routes: Set[Route] = Set.empty)
                     (implicit as: ActorSystem) extends BaseRouteRegistry {
 
     def bind(route: Route): Future[ServerBinding] = {
       implicit val am = ActorMaterializer()
       import scala.concurrent.ExecutionContext.Implicits.global
-      if (requestLogLevel == "off") {
-        Http().bindAndHandle(route, interface, port, ctx)
-      } else {
-        val myLoggedRoute = logRequestResult(requestLogLevel, requestLogFormat, route)
-        Http().bindAndHandle(myLoggedRoute, interface, port, ctx)
-      }
+      Http().bindAndHandle(route, interface, port, ctx)
     }
 
     def copyWith(p: Route) = {
@@ -247,109 +227,85 @@ class AkkaHttpModule extends Module with Initializable with Runnable with Dispos
   }
 
   def initialize(ctx: Context, cfg: Config, log: LoggingAdapter) = Try {
+
     import config._
 
     // create list of configuration tuples
     val mp = get[Map[String, Config]](AkkaHttpKey, cfg)
 
     val httpConfigs = if (mp.isEmpty) {
-      val r = HttpRouteRegistry(Nil, 9000, "localhost", default = true, "off", defaultLogFormat)(
+      val r = HttpRouteRegistry(Nil, 9000, "localhost", default = true)(
         ctx.inject[ActorSystem].getOrElse(throw InitializableError("Can't find default akka system")))
       List(r)
     } else {
       val autoDefault = mp.get.size == 1
-      mp.get.toList.filter(_._1 != RequestLogLevel).map { case (key, conf) =>
-        val system = config.get[String](AkkaAlias, conf).flatMap(ctx.inject[ActorSystem](_)).orElse(ctx.inject[ActorSystem])
-        if (system.isEmpty) {
-          throw InitializableError(s"Can't find akka system for http configuration key: $key")
-        }
-        val protocol = config.get[String](Protocol, conf).getOrElse("http")
-        val port = config.get[Int](Port, conf).getOrElse(-1)
-        val interface = config.get[String](Interface, conf).getOrElse("localhost")
-        val aliases = key :: config.get[List[String]](Aliases, conf).getOrElse(List.empty[String])
-        val default = config.get[Boolean](Default, conf).getOrElse(autoDefault)
-        val requestLogLevel: String = config.get[String](RequestLogLevel, conf).getOrElse("off")
-        val requestLogFormat: String = config.get[String](RequestLogFormat, conf).getOrElse(defaultLogFormat)
-        protocol.toLowerCase match {
-          case "http" =>
-            val r = HttpRouteRegistry(aliases, port, interface, default,
-              requestLogLevel, requestLogFormat)(system.get)
-            log.info(s"created: $r ")
-            r
-          case "https" =>
-            val r = HttpsRouteRegistry(aliases, port, interface, default, getHttpsConnectionContext(conf),
-              requestLogLevel, requestLogFormat)(system.get)
-            log.info(s"created: $r ")
-            r
-          case p => throw InitializableError(s"unknown protocol:$p in route registry, see: $config")
-        }
+      mp.get.toList.map {
+        case (key, conf) =>
+          val system = config.get[String](AkkaAlias, conf).flatMap(ctx.inject[ActorSystem](_)).orElse(ctx.inject[ActorSystem])
+          if (system.isEmpty) {
+            throw InitializableError(s"Can't find akka system for http configuration key: $key")
+          }
+          val protocol = config.get[String](Protocol, conf).getOrElse("http")
+          val port = config.get[Int](Port, conf).getOrElse(-1)
+          val interface = config.get[String](Interface, conf).getOrElse("localhost")
+          val aliases = key :: config.get[List[String]](Aliases, conf).getOrElse(List.empty[String])
+          val default = config.get[Boolean](Default, conf).getOrElse(autoDefault)
+          protocol.toLowerCase match {
+            case "http" =>
+              val r = HttpRouteRegistry(aliases, port, interface, default)(system.get)
+              log.info(s"created: $r ")
+              r
+            case "https" =>
+              val r = HttpsRouteRegistry(aliases, port, interface, default, getHttpsConnectionContext(conf))(system.get)
+              log.info(s"created: $r ")
+              r
+            case p => throw InitializableError(s"unknown protocol:$p in route registry, see: $config")
+          }
       }
     }
     val combinations = httpConfigs.groupBy(_.interface).map(_._2.groupBy(_.port).size).sum
     if (combinations != httpConfigs.size) {
       throw InitializableError(s"Akka http configuration contains ambiguous combination of port and protocol.")
     }
-    httpConfigs.foldLeft(ctx) { (ctx, cfg) =>
-      val ctx1 = if (cfg.default) {
-        ctx.register[RouteRegistry](cfg)
-      } else {
-        ctx
-      }
-      cfg.aliases.foldLeft(ctx1) { (ctx, alias) =>
-        ctx.register[RouteRegistry](cfg, Some(alias))
-      }
+    httpConfigs.foldLeft(ctx) {
+      (ctx, cfg) =>
+        val ctx1 = if (cfg.default) {
+          ctx.register[RouteRegistry](cfg)
+        } else {
+          ctx
+        }
+        cfg.aliases.foldLeft(ctx1) {
+          (ctx, alias) =>
+            ctx.register[RouteRegistry](cfg, Some(alias))
+        }
     }
   }
 
-  def logRequestResult(logLevel: String, logFormat: String, route: Route)
-                      (implicit ec: ExecutionContext): Route = {
-
-    def requestLog(request: HttpRequest, status: String, log: LoggingAdapter): String = {
-      val replacements: Map[String, String] = Map(
-        "HTTP_METHOD" -> request.method.value,
-        "REQ_URI" -> request.uri.toString(),
-        "RESP_STATUS" -> status
-      )
-
-      ParamsReplacer.replaceParams(logFormat, replacements, Some(log))
-    }
-
-    def myLoggingFunction(logger: LoggingAdapter)(req: HttpRequest)(res: Any): Unit = {
-
-      val level = Logging.levelFor(logLevel).getOrElse(Logging.DebugLevel)
-      val entry = res match {
-        case Complete(resp) =>
-          Future.successful(LogEntry(requestLog(req, resp.status.toString(), logger), level))
-        case Rejected(rejections) if rejections.isEmpty =>
-          Future.successful(LogEntry(requestLog(req, "404 Not Found", logger), level))
-        case Rejected(rejections) =>
-          Future.successful(LogEntry(requestLog(req, "400 Bad Request", logger), level))
-        case other => Future.successful(LogEntry(s"Other: $other", level))
-      }
-      entry.foreach(_.logTo(logger))
-    }
-    DebuggingDirectives.logRequestResult(LoggingMagnet(log => myLoggingFunction(log)))(route)
-  }
-
-  private def defaultLogFormat: String = "%{HTTP_METHOD} %{REQ_URI}: HTTP/%{RESP_STATUS}"
-
-  override def dependencies(dependencies: Dependency): Dependency = dependencies.&&[ConfigModule].&&[LogModule].&&[AkkaModule]
+  override def dependencies(dependencies: Dependency): Dependency =
+    dependencies.&&[ConfigModule].&&[LogModule].&&[AkkaModule]
 
   override def run(ctx: Context) = {
+
     import scala.concurrent.ExecutionContext.Implicits.global
+
     val log = ctx.inject[LoggingAdapterFactory].map(_ (this)).get
     val httpConfigs = ctx.registered[RouteRegistry].keySet.map(_.asInstanceOf[BaseRouteRegistry])
     val futures = httpConfigs.map(p => p().transform(p => p, th => RunnableError(s"Can`t initialize route $p", th)))
-    Future.sequence(futures).map { p =>
-      log.info(s"run: $httpConfigs")
-      ctx
+    Future.sequence(futures).map {
+      p =>
+        log.info(s"run: $httpConfigs")
+        ctx
     }
   }
 
   override def dispose(ctx: Context) = {
+
     import scala.concurrent.ExecutionContext.Implicits.global
+
     val futures = bindings.map(p => p.unbind().transform(p => p, th => DisposableError(s"Can`t initialize route $p", th)))
-    Future.sequence(futures).map { p => () }
+    Future.sequence(futures).map {
+      p => ()
+    }
   }
 
   private def getHttpsConnectionContext(cfg: Config): HttpsConnectionContext =
@@ -359,7 +315,9 @@ class AkkaHttpModule extends Module with Initializable with Runnable with Dispos
 
   private def getHttpsConnectionContextFormFactory(cfg: Config) = (clazzName: String) => {
     try {
+
       import scala.reflect.runtime.universe
+
       val runtimeMirror = universe.runtimeMirror(getClass.getClassLoader)
       val module = runtimeMirror.staticModule(clazzName)
       val companionObj = runtimeMirror.reflectModule(module).instance.asInstanceOf[HttpsConnectionContextFactory]
